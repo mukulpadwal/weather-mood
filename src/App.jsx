853810d60325a81569d31048a7acda6da6fb6b94
mukulpadwal <mukulpadwal.me@gmail.com>
2024-05-10 17:05:51 +0530

    FEAT : migration-from-cra-to-vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,52 @@
-import Search from './components/search/Search';
-import Forecast from './components/forecast/Forecast';
-import './App.css';
-import {useState } from 'react';
-import CurrentWeather from './components/current-weather/CurrentWeather';
-import { OPEN_WEATHER_BASE_URL, OPEN_WEATHER_FORECAST_URL } from "./api/openWeatherApi.js";
+import { useState } from "react";
+import Search from "@/components/Search";
+import Forecast from "@/components/Forecast";
+import CurrentWeather from "@/components/CurrentWeather.jsx";
+import {
+  OPEN_WEATHER_BASE_URL,
+  OPEN_WEATHER_FORECAST_URL,
+} from "@/api/openWeatherApi.js";
+import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const App = () => {
-
   const [weatherData, setWeatherData] = useState(null);
   const [forecastData, setForecastData] = useState(null);
 
-  function handleOnSearchChange(searchData){
+  function handleOnSearchChange(searchData) {
     const [lat, lon] = searchData.value.split(" ");
-    const currentWeatherFetch = fetch(`${OPEN_WEATHER_BASE_URL}&lat=${lat}&lon=${lon}`);
-    const forecastFetch = fetch(`${OPEN_WEATHER_FORECAST_URL}&lat=${lat}&lon=${lon}`);
+    const currentWeatherFetch = fetch(
+      `${OPEN_WEATHER_BASE_URL}&lat=${lat}&lon=${lon}`
+    );
+    const forecastFetch = fetch(
+      `${OPEN_WEATHER_FORECAST_URL}&lat=${lat}&lon=${lon}`
+    );
 
     Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (response) => {
         const weatherResponse = await response[0].json();
         const forecastResponse = await response[1].json();
 
-        setWeatherData({city: searchData.label, ...weatherResponse});
-        setForecastData({city: searchData.label, ...forecastResponse});
+        setWeatherData({ city: searchData.label, ...weatherResponse });
+        setForecastData({ city: searchData.label, ...forecastResponse });
       })
       .catch((error) => console.log(error));
-
   }
 
   return (
-    <div className='container'>  
-      <Search onSearchChange={handleOnSearchChange} />
-      {weatherData && <CurrentWeather weatherData={weatherData} />}
-      {forecastData && <Forecast forecastData={forecastData} />}
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Navbar />
+      <div className="p-4 w-full min-h-screen flex justify-center items-start">
+        <div className="w-11/12">
+          <Search onSearchChange={handleOnSearchChange} />
+          {weatherData && <CurrentWeather weatherData={weatherData} />}
+          {forecastData && <Forecast forecastData={forecastData} />}
+        </div>
+      </div>
+      <Footer />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
